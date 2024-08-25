96f15298214d9941990cdf186e732e5c1fb0b6cf
nmoran12 <99003530+nmoran12@users.noreply.github.com>
2024-08-25 23:15:14 +0930

    Add files via upload

--- Finder.cpp
+++ Finder.cpp
@@ -12,21 +12,20 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
   size_t index =
       0;  // This index lets us search from the beginning of the 's1' string
 
-  for (size_t i = 1; i <= s2.size(); i++) {
+  for (size_t i = 0; i <= s2.size(); ++i) {
     bool found = false;
     while (index < size1) {
       if (s1[index] == s2[i]) {
         result.push_back(index);
-        index = found +
-                1;  // This increments the index value so we are searching for
-                    // the next value at the next index, e.g. first iteration is
-                    // index = 0, next interation is index = 1, etc.
+        index++;  // This increments the index value so we are searching for
+                  // the next value at the next index, e.g. first iteration is
+                  // index = 0, next interation is index = 1, etc.
         found = true;
         break;
       }
       index++;
     }
-    if (found == false) {
+    if (!found) {
       result.push_back(-1);
     }
   }
