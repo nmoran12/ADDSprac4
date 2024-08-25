c92bbe8daa38ab9ccae5c110a09bf532c6982ba3
nmoran12 <99003530+nmoran12@users.noreply.github.com>
2024-08-25 23:09:51 +0930

    Add files via upload

---
+++ Finder-1.h
@@ -0,0 +1,13 @@
+#ifndef FINDER_H
+#define FINDER_H
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Finder {
+ public:
+  std::vector<int> findSubstrings(std::string s1, std::string s2);
+};
+
+#endif
